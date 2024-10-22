6bafd6b5da1a8d7956d037762787abcf9ba0f1cc
ramos-adrian <adrian@ramos.ar>
2024-10-22 19:05:54 -0300

    Add superadmin initializer

--- src/main/java/ar/ramos/girsugps/internal/user/AdminUserInitializer.java
+++ src/main/java/ar/ramos/girsugps/internal/user/AdminUserInitializer.java
@@ -1,10 +1,13 @@
 package ar.ramos.girsugps.internal.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Profile("dev")
 @Component
 public class AdminUserInitializer implements CommandLineRunner {
@@ -16,14 +19,18 @@ public class AdminUserInitializer implements CommandLineRunner {
     public AdminUserInitializer(
             JpaUserDetailsService userService,
             @Value("${ADMIN_USERNAME}") String ADMIN_USERNAME,
-            @Value("${ADMIN_PASSWORD") String ADMIN_PASSWORD) {
+            @Value("${ADMIN_PASSWORD}") String ADMIN_PASSWORD) {
         this.userService = userService;
         this.ADMIN_USERNAME = ADMIN_USERNAME;
         this.ADMIN_PASSWORD = ADMIN_PASSWORD;
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        userService.addUser(ADMIN_USERNAME, ADMIN_PASSWORD, "ROLE_ADMIN");
+    public void run(String... args) {
+        try {
+            userService.addUser(ADMIN_USERNAME, ADMIN_PASSWORD, "ROLE_ADMIN");
+        } catch (DataIntegrityViolationException e) {
+            log.info("Admin user already exists");
+        }
     }
 }
